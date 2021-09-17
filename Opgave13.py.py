7eaf94f87d5b74e041425da90f2ac1955fd5eec6
ZyaadW <90029140+ZyaadW@users.noreply.github.com>
2021-09-17 09:28:56 +0200

    Add files via upload

---
+++ Opgave13.py.py
@@ -0,0 +1,8 @@
+print("""
+<svg version="1.1"
+    baseProfile="full"
+    width="300" height="200"
+    xmlns="http://www.w3.org/2000/svg">
+    <circle cx="150" cy="100" r="80" fill="green" />
+    <rect x="0" y="20" width="80" height="40" fill="red" />
+</svg>""")
