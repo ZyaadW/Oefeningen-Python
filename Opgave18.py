d9547d23f13750c7c24e04a613cc18bfd9ffc69c
ZyaadW <90029140+ZyaadW@users.noreply.github.com>
2021-10-01 09:51:30 +0200

    Add files via upload

---
+++ Opgave18.py
@@ -0,0 +1,11 @@
+print("""<svg version="1.1"
+    baseProfile="full"
+    width="300" height="200"
+    xmlns="http://www.w3.org/2000/svg">""")
+for i in range(0,100) :
+
+    print(f"""
+    <circle cx="{i}" cy="{i}" r="80" fill="green" />""")
+    
+    
+print("</svg>")
