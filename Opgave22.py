63dbd98479adf01ff69265215243237f71b34142
ZyaadW <90029140+ZyaadW@users.noreply.github.com>
2021-10-15 09:44:19 +0200

    Add files via upload

---
+++ Opgave22.py
@@ -0,0 +1,6 @@
+import random
+pincode= random.randint(0,9)
+pincode2= random.randint(0,9)
+pincode3= random.randint(0,9)
+pincode4= random.randint(0,9)
+print(f"{pincode}{pincode2}{pincode3}{pincode4}")
